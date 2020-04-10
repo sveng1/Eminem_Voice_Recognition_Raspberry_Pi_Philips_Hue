f2c7af52ee96660ba8c7c353e58f5050e500fc73
sveng1 <selina.veng@gmail.com>
2020-04-10 19:35:32 +0200

    added main script

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-import time
 from tensorflow.keras.models import load_model
-import sounddevice as sd
+import time
 from utils import audio2spectrogram
-from hue_functions import set_color_all
+import sounddevice as sd
+from utils import audio2spectrogram, eminem_light
+import numpy as np
+from hue_functions import get_light_state, get_connected_lights
+from user import bridge_url, hue_user
 
 
-# Load trained crnn model
-model_path = '/data/best_model_3_sec.h5'
+model_path = 'crnn.h5'
 model = load_model(model_path)
+print('Loaded model from "{}".'.format(model_path))
 
-
-# Philips Hue bridge url and user id
-bridge_url = 'http://192.168.0.42'
-user = 'UEDVry7d0T4WYiNW58fA61pZ-dixxHkMhjtz3bQl'
-
-# Values for time loop
 time_step = 6
 starttime=time.time()
 
-# Values for recording
 sr = 16000
 seconds = 3
 frames = int(seconds * sr)
+print('Sampling rate: {}, recording length: {} seconds'.format(sr, seconds))
 
+# Get current hue and saturation value for each connected light
+lights = get_connected_lights(bridge_url, hue_user)
+previous_color = [[get_light_state(l)[key] for key in ['hue', 'sat']] for l in lights]
+state = 'not eminem'
+
+print('Started recording')
 try:
     while True:
-        print(time.ctime())
+        print('Time:', time.ctime())
         # Record audio
         recording = sd.rec(frames=frames, samplerate=sr, channels=1)
         sd.wait()
@@ -35,10 +38,14 @@ try:
         spec = audio2spectrogram(recording)
 
         # Predict eminem or not eminem
-        prediction = np.argmax(model.predict(spec)[0])
+        prediction = np.random.randint(2, size=1)
+        #prediction = np.argmax(model.predict(spec)[0])
 
         # Use prediction to set light
-        eminem_light(bridge_url, user, previous_color, prediction)
+        if not (state == 'not eminem' and prediction == 0):
+            state, previous_color = eminem_light(bridge_url, hue_user, prediction, state, previous_color)
+            time.sleep(time_step - ((time.time() - starttime) % time_step))
+        print('Detecting:', state)
 
         time.sleep(time_step - ((time.time() - starttime) % time_step))
 except KeyboardInterrupt:
